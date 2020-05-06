887aa5f9266e407f160da33196922b6256d895d1
游轶 <youyi@forchange.tech>
2020-05-06 15:32:28 +0800

    feat: blog update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 //用于网站结构配置
 const nav = require('./nav');
 const sidebar = require('./sideBar')
-let base = ''
+let base = '/TechBlog/'
 module.exports = {
 	title: 'Cecilia You',
 	description: '自由、利他、体验，寻找使命。',
